014d01b3dfec575762ae1577a9e15df9f1fb8d42
e29denghy <42734828@qq.com>
2018-06-07 03:38:06 +0000

    com notify 7

--- resources/views/course/notify.blade.php
+++ resources/views/course/notify.blade.php
@@ -15,7 +15,7 @@
       <br>
   </div>
   <div class="pdr_0 mt-3">
-    <img class="featurette-image img-fluid" src="/image/dswl.jpg" alt="Generic placeholder image">
+    <img class="featurette-image img-fluid" src="/image/dswl.jpg" alt="邓宏伟老师与都市物流学员集体合影">
   </div>
   <div class="row">
     <div class="p-4">
@@ -35,7 +35,7 @@
       </div>
   </div>
   <div class="pdr_0 mt-3 mb-3">
-    <img class="img-fluid p-0" src="/image/tx.jpg" alt="Generic placeholder image">
+    <img class="img-fluid p-0" src="/image/tx.jpg" alt="邓宏伟老师与天禧国际学员集体合影">
   </div>
   <hr class="featurette-divider">
   <div class="row featurette bg-danger">
@@ -47,7 +47,7 @@
     </div>
   </div>
   <div class="pdr_0 mt-3 mb-3">
-    <img class="featurette-image img-fluid" src="/image/gain.jpg" alt="Generic placeholder image">
+    <img class="featurette-image img-fluid" src="/image/gain.jpg" alt="邓宏伟老师与神州通学员集体合影">
   </div>
   <div class="row featurette bg-danger p-3 mb-3" id="go">
     <div class="justify-content-center">
@@ -56,6 +56,24 @@
       <p><h5 class="let_3 text-white"><span data-feather="home"></span>{{ $data['address'] }}</h5></p>
       <p><h5 class="let_3 text-white"><span data-feather="map"></span>{{ $data['metro'] }}</h5></p>
       <p><h5 class="let_3 text-white"><span data-feather="map-pin"></span>{{ $data['public_bus'] }}</h5></p>
+    </div>
+  </div>
+  <div class="pdr_0 mt-3 mb-3">
+    <img class="featurette-image img-fluid" src="/image/tx2.jpg" alt="邓宏伟老师与天禧国际获奖学员合影">
+  </div>
+  <div class="row featurette bg-danger p-3 mb-3" id="go">
+    <div class="justify-content-center">
+      <h2 class="let_3 text-center mt-3 mb-3 text-white">收费标准</h2>
+        <p>
+          <h5 class="text-white let_2"><span data-feather="thumbs-up"></span>{{ $data['pay_1'] }}</h5>
+          <h5 class="text-white let_2"><span data-feather="thumbs-up"></span>{{ $data['pay_2'] }}</h5>
+        </p>
+      <h2 class="let_3 text-center mt-3 mb-3 text-white">现场福利</h2>
+        <p>
+          <h5 class="text-white let_2"><span data-feather="dollar-sign"></span>{{ $data['bonus_1'] }}</h5>
+          <h5 class="text-white let_2"><span data-feather="gift"></span>{{ $data['bonus_2'] }}</h5>
+          <h5 class="text-white let_2"><span data-feather="gift"></span>{{ $data['bonus_3'] }}</h5>
+        </p>
     </div>
   </div>
   <hr class="featurette-divider">
@@ -96,7 +114,7 @@
       <br>
   </div>
   <div class="pdr_0 mt-3">
-    <img class="featurette-image img-fluid" src="/image/dhw.jpg" alt="Generic placeholder image">
+    <img class="featurette-image img-fluid" src="/image/dhw.jpg" alt="邓宏伟老师个人照片">
   </div>
   <div class="row featurette p-3">
     <p class="lead"><br>邓老师1998年毕业于山东大学经济学院经济学系，拥有十多年知名企业的文化建设工作经验和七年培训咨询工作经验，任职和服务过的上市公司和知名企业包括：海能达(002583)、富安娜(002327)、华孚时尚(002042)、爱施德(002416)、神州通集团、丽斯达、天玺国际、瑞捷咨询、利达旺电梯、某集团37.5℃运营中心、等候科技、泰天恒集团、正雄鞋材、建顺包材、国乐飘香、番茄假期、桑菲通信、中创软件、得安科技、正一道场、青青宝贝、都市物流等。</p>
